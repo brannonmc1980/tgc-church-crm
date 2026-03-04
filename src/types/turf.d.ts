fb0f0578cc539914881747d95d7b46e6c38db848
Brannon McAllister <brannon@Brannons-Mac-Studio.local>
2026-03-04 08:28:10 -0500

    Add type declarations for @turf/helpers and @turf/boolean-point-in-polygon

--- src/types/turf.d.ts
+++ src/types/turf.d.ts
@@ -1,3 +1,27 @@
+declare module "@turf/helpers" {
+  export interface Feature<G = Geometry, P = Record<string, unknown>> {
+    type: "Feature";
+    geometry: G;
+    properties: P | null;
+    id?: string | number;
+  }
+  export interface Point {
+    type: "Point";
+    coordinates: number[];
+  }
+  export interface Polygon {
+    type: "Polygon";
+    coordinates: number[][][];
+  }
+  export interface MultiPolygon {
+    type: "MultiPolygon";
+    coordinates: number[][][][];
+  }
+  export type Geometry = Point | Polygon | MultiPolygon;
+  export function point(coordinates: number[], properties?: Record<string, unknown>): Feature<Point>;
+  export function polygon(coordinates: number[][][], properties?: Record<string, unknown>): Feature<Polygon>;
+}
+
 declare module "@turf/boolean-point-in-polygon" {
   import { Feature, Point, Polygon, MultiPolygon } from "@turf/helpers";
   function booleanPointInPolygon(
